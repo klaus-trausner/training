5f6de6839ff2373305429f448441fcd4ef395552
Klaus Trausner <trausnerk@gmail.com>
2025-04-21 18:55:53 +0200

    letzte änderungen

--- boot.py
+++ boot.py
@@ -1,17 +1,17 @@
 # boot.py -- run on boot-up
 import esp32wlan
-import senko
-import machine
+#import senko
+#import machine
 
 esp32wlan.wifi.do_connect()
 
-OTA = senko.Senko(user="klaus-trausner", repo="training",  branch ="main", working_dir="", files=["testdatei.txt","testdatei2.txt", "boot.py", "main.py"])
-print("OTA Update: ", OTA.update())
-
+#OTA = senko.Senko(user="klaus-trausner", repo="training",  branch ="main", working_dir="", files=["testdatei.txt","testdatei2.txt", "boot.py", "main.py"])
+#print("OTA Update: ", OTA.update())
+"""
 if OTA.update():
     print("Updated to the latest version! Rebooting...")
     machine.reset()
 else:
     print("No update available.")
-
+"""
 print("boot beendet!")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#from platform import machine
 from machine import Pin, SoftI2C, Timer      
-#from ssd1306 import SSD1306_I2C  
+#from ssd1306 import SSD1306_I2C 
+import urequests as requests 
 import bme280
 import time
 #import network
@@ -8,6 +10,8 @@ import time
 #import esp32wlan
 import esp32display
 import mqtt
+import os
+#import ubinascii
 
 
 
@@ -16,6 +20,8 @@ import mqtt
 #i2c_bme280 = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
 i2c_bme280 = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)
 bme = bme280.BME280(i2c=i2c_bme280)
+mqttc = mqtt.mqttc
+reconnected = 0
 
 
 
@@ -39,8 +45,34 @@ def writeMessures(temp, hum, pres):
 
 def sub_cb(topic, msg):
   print((topic, msg))
-#esp32wlan.wifi.do_connect()
+  #t = str(topic, encoding='utf-8')
+  #print("t: ",t)
+  
+  if topic == b"firmware":
+      time.sleep(1)
+      makeResponse(topic, msg)
+  elif topic == b"timer":
+      getTimeDatas(msg)
+  else:
+      print("Topic nicht bekannt!")
+      
+def makeResponse(topic, msg):      
+    url = msg.decode().split(",")
+    print("url: ", url[0], "\nDatei: ", url[1])
+    response = requests.get(url[0])
+    print("status: ", response.status_code)
+    if response.status_code == 200:
 
+      with open(url[1], "w") as f:
+        f.write(response.text)
+        print("Update erfolgreich!")
+    else:
+      print("Update fehlgeschlagen!")
+
+    response.close()
+
+def getTimeDatas(msg): 
+   print("Get Time Datas: ", msg)  
 
   
 def publishSensors(t):
@@ -50,22 +82,41 @@ def publishSensors(t):
   mqttc.publish("test", hum)
   mqttc.publish("test", pres)
   mqttc.publish("innen",temp+"-"+hum+"-"+pres)
-  time.sleep(10)
-
-mqttc = mqtt.mqttc
-mqttc.connect()
-mqttc.publish("test", "new mwssage from micropython...")
-mqttc.set_callback(sub_cb)
-mqttc.subscribe("test")
+  
 
 
+def connectMQTT():
+  global reconnected
+  reconnected=reconnected+1
+  print("reconncted: ",reconnected)
+  if reconnected>10:
+    print("MQTT-Verbindung fehlgeschlagen!, System wird geendet!")
+    exit()
 
-timerSensors=Timer(period=10000, mode=Timer.PERIODIC, callback=publishSensors)
+  #mqttc.disconnect()
+  mqttc.set_callback(sub_cb)
+  mqttc.connect()
+  mqttc.publish("test", "new mwssage from micropython...")
+  
+  mqttc.subscribe(b"firmware")
+  mqttc.subscribe(b"timer")
+
+connectMQTT()
+print("Timer wird gestartet!")
+timerSensors=Timer(1)
+timerSensors.init(period=60000, mode=Timer.PERIODIC, callback=publishSensors)
 print("Loop startet....")
+print("Updated--2--!!")
+
+
 while True:
-  try:
-    mqttc.check_msg()
-  except:
-    print("MQTT-Error")
-    pass
+    if True:
+        mqttc.wait_msg()
+    else:
+        mqttc.check_msg()
+        time.sleep(1)
+
+mqttc.disconnect()
+    
+    
   
